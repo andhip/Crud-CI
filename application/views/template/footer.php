829eea77fd6062f31d49618a6772c8e5adac653a
andhip <andhi.puspianto10@gmail.com>
2021-02-21 23:10:44 +0700

    update new files

--- application/views/template/footer.php
+++
@@ -1,8 +0,0 @@
-	<footer class="footer">
-	  <div class="container" style="text-align:center;">
-	  	<hr/>
-        <span>Warung Belajar @<?php echo date('Y'); ?></span>
-      </div>
-    </footer>
-  </body>
-</html>
